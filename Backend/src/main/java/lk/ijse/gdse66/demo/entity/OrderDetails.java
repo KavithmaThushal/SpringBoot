d09fabbc02572baedb004ecf9f384ac51c22199c
KavithmaThushal <kaviyabro9007@gmail.com>
2025-04-28 16:24:41 +0530

    Orders and OrderDetails Added

---
+++ Backend/src/main/java/lk/ijse/gdse66/demo/entity/OrderDetails.java
@@ -0,0 +1,31 @@
+package lk.ijse.gdse66.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
+public class OrderDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int quantity;
+    private double unitPrice;
+    private double total;
+
+    @ManyToOne
+    @NotNull
+    private Orders order;
+
+    @ManyToOne
+    @NotNull
+    private Item item;
+}
---
+++ Backend/src/main/java/lk/ijse/gdse66/demo/entity/Orders.java
@@ -0,0 +1,31 @@
+package lk.ijse.gdse66.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private LocalDate date;
+
+    @ManyToOne
+    @NotNull
+    private Customer customer;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderDetails> orderDetails;
+}
